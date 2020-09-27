2eed5ff48860e8e48c3c37d9f9885d383ec0f8f3
Deyvi Tineo <50240102+deyvidoes@users.noreply.github.com>
2020-09-27 12:14:54 -0400

    Added settings activity

--- app/src/main/java/com/example/news/ui/MainActivity.java
+++ app/src/main/java/com/example/news/ui/MainActivity.java
@@ -1,12 +1,14 @@
-package com.example.news;
+package com.example.news.ui;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.news.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
@@ -15,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
 }
--- app/src/main/java/com/example/news/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/news/ui/home/HomeFragment.java
@@ -1,13 +1,21 @@
 package com.example.news.ui.home;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,7 +23,9 @@ import com.example.news.NewsApp;
 import com.example.news.R;
 import com.example.news.adapters.NewsAdapter;
 import com.example.news.models.Article;
+import com.example.news.ui.SettingsActivity;
 import com.example.news.util.Constants;
+import com.example.news.util.CountryAbbreviationConverter;
 import com.example.news.viewmodels.NewsViewModel;
 
 import java.util.ArrayList;
@@ -26,6 +36,7 @@ import javax.inject.Inject;
 public class HomeFragment extends Fragment {
     private NewsAdapter mRecyclerAdapter;
     private List<Article> mArticles = new ArrayList<>();
+    private TextView mCountry;
 
     @Inject
     NewsViewModel mNewsViewModel;
@@ -33,10 +44,23 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         ((NewsApp) requireActivity().getApplication()).getAppComponent().inject(this);
+
+        initWidgets(view);
         initRecyclerView(view);
         updateData();
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(view.findViewById(R.id.toolbar));
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    private void initWidgets(View view){
+        mCountry = view.findViewById(R.id.tv_country);
     }
 
     private void initRecyclerView(View view) {
@@ -46,9 +70,18 @@ public class HomeFragment extends Fragment {
         mRecyclerView.setAdapter(mRecyclerAdapter);
     }
 
-    //TODO: Allow user to pick different countries
     private void updateData() {
-        mNewsViewModel.getTopNewsData(Constants.COUNTRY_US).observe(getViewLifecycleOwner(), newsResponse -> {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(requireActivity());
+        String country = sp.getString("country", Constants.COUNTRY_US);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        sb.append(CountryAbbreviationConverter.getCountryFromAbbreviation(country));
+        sb.append(")");
+
+        mCountry.setText(sb.toString());
+
+        mNewsViewModel.getTopNewsData(country).observe(getViewLifecycleOwner(), newsResponse -> {
             mArticles.clear();
             mArticles.addAll(newsResponse.getArticles());
             mRecyclerAdapter.notifyDataSetChanged();
@@ -58,4 +91,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateData();
+    }
+
+    //category
+    //The category you want to get headlines for. Possible options: business entertainment
+    // general health science sports technology .
+    // Note: you can't mix this param with the sources param.
+    //TODO: add category picker
 }
